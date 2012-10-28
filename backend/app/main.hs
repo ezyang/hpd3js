82e63bc716d3d0925854f5960d95826ca77d94a7
Edward Z. Yang <ezyang@mit.edu>
2012-10-28 15:46:33 -0700

    Setup FastCGI.
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- backend/app/main.hs
+++ backend/app/main.hs
@@ -3,6 +3,11 @@ import Yesod.Default.Config (fromArgs)
 import Yesod.Default.Main   (defaultMain)
 import Settings             (parseExtra)
 import Application          (makeApplication)
+import Network.Wai.Handler.FastCGI (run)
+import Control.Monad
+
+-- main :: IO ()
+-- main = defaultMain (fromArgs parseExtra) makeApplication
 
 main :: IO ()
-main = defaultMain (fromArgs parseExtra) makeApplication
+main = fromArgs parseExtra >>= makeApplication >>= run
