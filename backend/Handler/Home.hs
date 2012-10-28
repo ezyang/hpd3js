7134a6cd70542d29c30351710a9aa4e0c96e4071
Edward Z. Yang <ezyang@mit.edu>
2012-10-27 18:23:05 -0700

    Add times and a front page listing.
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- backend/Handler/Home.hs
+++ backend/Handler/Home.hs
@@ -11,6 +11,14 @@ import qualified Data.Text.Encoding as Text
 import qualified Data.ByteString.Base16 as Base16
 import System.FilePath
 import qualified Data.Text as Text
+import Data.Time
+import System.Locale (defaultTimeLocale)
+
+dateTimeFormat :: String
+dateTimeFormat = "%e %b %Y %H:%M:%S"
+
+format :: UTCTime -> String
+format = formatTime defaultTimeLocale dateTimeFormat
 
 -- no 'staticFiles' splice, since the contents of 'uploaded' will always
 -- be dynamic
@@ -21,6 +29,7 @@ uploadDirectory = "uploaded"
 getHomeR :: Handler RepHtml
 getHomeR = do
     (formWidget, formEnctype) <- generateFormPost sampleForm
+    profiles <- runDB $ selectList [] [Desc ProfileTime, LimitTo 10]
     let handlerName = "getHomeR" :: Text
     defaultLayout $ do
         aDomId <- lift newIdent
@@ -33,7 +42,7 @@ getViewR pid = do
     defaultLayout $ do
         setTitle . toHtml $ profileTitle profile
         [whamlet|<h1>#{profileTitle profile}
-                 <p>Hash is #{profileHash profile}|]
+                 <p>Hash is #{profileHash profile} uploaded on #{format (profileTime profile)}|]
 
 postUploadR :: Handler RepHtml
 postUploadR = do
@@ -42,10 +51,11 @@ postUploadR = do
         FormSuccess (file, title) -> do
             -- XXX Need to check if the file is legit to avoid spamming
             bhash :: SHA1 <- liftIO . runResourceT $ fileSource file $$ sinkHash
-            let hash = Text.decodeUtf8 (Base16.encode (encode bhash))
+            let hash = Text.decodeUtf8 . Base16.encode $ encode bhash
             _ <- liftIO $ evaluate hash
+            currentTime <- liftIO getCurrentTime
             liftIO $ fileMove file (uploadDirectory </> Text.unpack hash)
-            uploadId <- runDB . insert $ Profile title hash
+            uploadId <- runDB . insert $ Profile title hash currentTime
             redirect (ViewR uploadId)
         _ -> defaultLayout $ do
             setTitle "Unable to upload file"
